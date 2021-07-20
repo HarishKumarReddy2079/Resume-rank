3e897efe898bfa893b3fd5f1c3eb135e14749eed
bhavesh <bhavesh@perfectvips.com>
2021-07-20 13:13:18 +0530

    ci yml file added

--- app.py
+++ app.py
@@ -308,5 +308,5 @@ def custom_static1(filename):
 if __name__ == '__main__':
    # app.run(debug = True) 
     # app.run('127.0.0.1' , 5000 , debug=True)
-    app.run(host='0.0.0.0', debug=True , threaded=True, port=int(os.environ.get('PORT',8080)))
+    app.run(host='0.0.0.0', debug=True , threaded=True, port=int(os.environ.get('PORT',3000)))
     
